8fa79d3e7267aba4b712e44f2a99b86be92e0e52
Joecheong2006 <joecheong2006@gmail.com>
2025-05-25 17:52:31 -0700

    add QuadRenderer.py

---
+++ src/QuadRenderer.py
@@ -0,0 +1,116 @@
+from opengl_util import *
+
+class Quad:
+    def __init__(self, size: tuple[float, float] = (1 ,1), pos: tuple[float, float] = (0, 0), angle: float = 0):
+        self.size = size
+        self.pos = pos
+        self.angle = angle
+
+class QuadRenderer:
+    def __init__(self, window: Window, quads_size: int = 4000):
+        self.quads_size = quads_size
+        self.vertexBuff = np.zeros(quads_size, dtype=np.float32)
+        self.indexBuff = np.zeros(quads_size, dtype=np.uint32)
+        self.window = window
+
+        self.vao = glGenVertexArrays(1)
+        self.vbo = glVertexBuffer(None, 0, GL_DYNAMIC_DRAW);
+
+        glBindVertexArray(self.vao)
+
+        stride = 5 * 4
+        # Position (location = 0)
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, ctypes.c_void_p(0))
+
+        # UV (location = 1)
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, ctypes.c_void_p(3 * 4))
+
+        self.ibo = glIndexBuffer(None, 0);
+        self.vbIndex = 0
+        self.ibIndex = 0
+        self.full = False
+
+    def clearBuffer(self):
+        self.vbIndex = 0
+        self.ibIndex = 0
+        self.full = False
+
+    def drawQuad(self, q: Quad):
+        if self.vbIndex >= self.quads_size - 12 - 8:
+            self.full = True
+            return
+
+        if self.vbIndex >= self.quads_size - 6:
+            self.full = True
+            return
+
+        ratioy = self.window.height / self.window.width
+        size = np.array(q.size, dtype=np.float32) * 0.5
+        pos = np.array([q.pos[0] * ratioy, q.pos[1]], dtype=np.float32)
+
+        cur = self.vbIndex / 5;
+        self.indexBuff[self.ibIndex] = cur + 0;
+        self.indexBuff[self.ibIndex + 1] = cur + 1;
+        self.indexBuff[self.ibIndex + 2] = cur + 3;
+        self.indexBuff[self.ibIndex + 3] = cur + 0;
+        self.indexBuff[self.ibIndex + 4] = cur + 2;
+        self.indexBuff[self.ibIndex + 5] = cur + 3;
+        self.ibIndex += 6;
+
+        # Memory Layout: 
+        #   (loc = 0) position: 3 x f32 = 12bytes
+        #   (loc = 1) uv: 2 x f32 = 8bytes
+
+        c, s = np.cos(q.angle), np.sin(q.angle)
+        mat = np.array([
+            [c, -s],
+            [s,  c]
+        ], dtype=np.float32);
+
+        v1 = mat @ np.array([-size[0], -size[1]], dtype=np.float32)
+        v2 = mat @ np.array([-size[0], size[1]], dtype=np.float32)
+
+        self.vertexBuff[self.vbIndex] = pos[0] + v1[0] * ratioy;
+        self.vertexBuff[self.vbIndex + 1] = pos[1] + v1[1];
+        self.vertexBuff[self.vbIndex + 2] = 0;
+
+        self.vertexBuff[self.vbIndex + 3] = 0;
+        self.vertexBuff[self.vbIndex + 4] = 0;
+
+        self.vertexBuff[self.vbIndex + 5] = pos[0] + v2[0] * ratioy;
+        self.vertexBuff[self.vbIndex + 6] = pos[1] + v2[1];
+        self.vertexBuff[self.vbIndex + 7] = 0;
+
+        self.vertexBuff[self.vbIndex + 8] = 0;
+        self.vertexBuff[self.vbIndex + 9] = 1;
+
+        self.vertexBuff[self.vbIndex + 10] = pos[0] - v2[0] * ratioy;
+        self.vertexBuff[self.vbIndex + 11] = pos[1] - v2[1];
+        self.vertexBuff[self.vbIndex + 12] = 0;
+
+        self.vertexBuff[self.vbIndex + 13] = 1;
+        self.vertexBuff[self.vbIndex + 14] = 0;
+
+        self.vertexBuff[self.vbIndex + 15] = pos[0] - v1[0] * ratioy;
+        self.vertexBuff[self.vbIndex + 16] = pos[1] - v1[1];
+        self.vertexBuff[self.vbIndex + 17] = 0;
+
+        self.vertexBuff[self.vbIndex + 18] = 1;
+        self.vertexBuff[self.vbIndex + 19] = 1;
+        self.vbIndex += 20;
+
+    def submit(self):
+        self.vbo.setBuffer(self.vertexBuff, self.vertexBuff.nbytes)
+        self.ibo.setBuffer(self.indexBuff, self.indexBuff.size)
+
+        glBindVertexArray(self.vao)
+        glDrawElements(GL_TRIANGLES, self.ibIndex, GL_UNSIGNED_INT, None)
+        self.vbIndex = 0;
+        self.ibIndex = 0;
+
+    def delete(self):
+        glDeleteVertexArrays(1, [self.vao])
+        self.vbo.delete()
+        self.ibo.delete()
--- src/opengl_util.py
+++ src/opengl_util.py
@@ -1,6 +1,7 @@
 from Window import Window
 from OpenGL.GL import *
 import numpy as np
+from PIL import Image
 
 class glVertexBuffer:
     def __init__(self, data, size: int, usage: int):
@@ -79,16 +80,30 @@ class glShaderProgram:
         glDeleteProgram(self.program);
 
 class glTexture:
-    def __init__(self, width: int, height: int, style: int, data = None):
+    @staticmethod
+    def loadTexture(path: str, style: int):
+        image = Image.open(path)
+        mode = image.mode  # "RGB" or "RGBA" or others
+        if mode not in ["RGB", "RGBA"]:
+            image = image.convert("RGBA")  # safest fallback
+
+        # Flip image vertically
+        img_data = np.array(image)[::-1]
+
+        # Determine format
+        format = GL_RGBA if image.mode == "RGBA" else GL_RGB        
+        return glTexture(image.width, image.height, style, format, img_data)
+
+    def __init__(self, width: int, height: int, style: int, format: int = GL_RGBA, data = None):
         self._id = glGenTextures(1)
         self.width = width
         self.height = height
         self.bind()
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data)
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data)
 
         glGenerateMipmap(GL_TEXTURE_2D)
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, style)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, style)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
@@ -145,118 +160,3 @@ class glFramebuffer:
         glDeleteVertexArrays(1, [self.quad_vao])
         glDeleteBuffers(1, [self.quad_vbo])
         glDeleteFramebuffers(1, [self.fbo])
-
-class Quad:
-    def __init__(self, size: tuple[float, float] = (1 ,1), pos: tuple[float, float] = (0, 0), angle: float = 0):
-        self.size = size
-        self.pos = pos
-        self.angle = angle
-
-class QuadRenderer:
-    def __init__(self, window: Window, quads_size: int = 4000):
-        self.quads_size = quads_size
-        self.vertexBuff = np.zeros(quads_size, dtype=np.float32)
-        self.indexBuff = np.zeros(quads_size, dtype=np.uint32)
-        self.window = window
-
-        self.vao = glGenVertexArrays(1)
-        self.vbo = glVertexBuffer(None, 0, GL_DYNAMIC_DRAW);
-
-        glBindVertexArray(self.vao)
-
-        stride = 5 * 4
-        # Position (location = 0)
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, ctypes.c_void_p(0))
-
-        # UV (location = 1)
-        glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, ctypes.c_void_p(3 * 4))
-
-        self.ibo = glIndexBuffer(None, 0);
-        self.vbIndex = 0
-        self.ibIndex = 0
-        self.full = False
-
-    def clearBuffer(self):
-        self.vbIndex = 0
-        self.ibIndex = 0
-        self.full = False
-
-    def drawQuad(self, q: Quad):
-        if self.vbIndex >= self.quads_size - 12 - 8:
-            self.full = True
-            return
-
-        if self.vbIndex >= self.quads_size - 6:
-            self.full = True
-            return
-
-        ratioy = self.window.height / self.window.width
-        size = np.array(q.size, dtype=np.float32) * 0.5
-        pos = np.array([q.pos[0] * ratioy, q.pos[1]], dtype=np.float32)
-
-        cur = self.vbIndex / 5;
-        self.indexBuff[self.ibIndex] = cur + 0;
-        self.indexBuff[self.ibIndex + 1] = cur + 1;
-        self.indexBuff[self.ibIndex + 2] = cur + 3;
-        self.indexBuff[self.ibIndex + 3] = cur + 0;
-        self.indexBuff[self.ibIndex + 4] = cur + 2;
-        self.indexBuff[self.ibIndex + 5] = cur + 3;
-        self.ibIndex += 6;
-
-        # Memory Layout: 
-        #   (loc = 0) position: 3 x f32 = 12bytes
-        #   (loc = 1) uv: 2 x f32 = 8bytes
-
-        c, s = np.cos(q.angle), np.sin(q.angle)
-        mat = np.array([
-            [c, -s],
-            [s,  c]
-        ], dtype=np.float32);
-
-        v1 = mat @ np.array([-size[0], -size[1]], dtype=np.float32)
-        v2 = mat @ np.array([-size[0], size[1]], dtype=np.float32)
-
-        self.vertexBuff[self.vbIndex] = pos[0] + v1[0] * ratioy;
-        self.vertexBuff[self.vbIndex + 1] = pos[1] + v1[1];
-        self.vertexBuff[self.vbIndex + 2] = 0;
-
-        self.vertexBuff[self.vbIndex + 3] = 0;
-        self.vertexBuff[self.vbIndex + 4] = 0;
-
-        self.vertexBuff[self.vbIndex + 5] = pos[0] + v2[0] * ratioy;
-        self.vertexBuff[self.vbIndex + 6] = pos[1] + v2[1];
-        self.vertexBuff[self.vbIndex + 7] = 0;
-
-        self.vertexBuff[self.vbIndex + 8] = 0;
-        self.vertexBuff[self.vbIndex + 9] = 1;
-
-        self.vertexBuff[self.vbIndex + 10] = pos[0] - v2[0] * ratioy;
-        self.vertexBuff[self.vbIndex + 11] = pos[1] - v2[1];
-        self.vertexBuff[self.vbIndex + 12] = 0;
-
-        self.vertexBuff[self.vbIndex + 13] = 1;
-        self.vertexBuff[self.vbIndex + 14] = 0;
-
-        self.vertexBuff[self.vbIndex + 15] = pos[0] - v1[0] * ratioy;
-        self.vertexBuff[self.vbIndex + 16] = pos[1] - v1[1];
-        self.vertexBuff[self.vbIndex + 17] = 0;
-
-        self.vertexBuff[self.vbIndex + 18] = 1;
-        self.vertexBuff[self.vbIndex + 19] = 1;
-        self.vbIndex += 20;
-
-    def submit(self):
-        self.vbo.setBuffer(self.vertexBuff, self.vertexBuff.nbytes)
-        self.ibo.setBuffer(self.indexBuff, self.indexBuff.size)
-
-        glBindVertexArray(self.vao)
-        glDrawElements(GL_TRIANGLES, self.ibIndex, GL_UNSIGNED_INT, None)
-        self.vbIndex = 0;
-        self.ibIndex = 0;
-
-    def delete(self):
-        glDeleteVertexArrays(1, [self.vao])
-        self.vbo.delete()
-        self.ibo.delete()
