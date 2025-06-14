458770ef07d25e6ed105d92f4b166f435efcadf9
Joecheong2006 <joecheong2006@gmail.com>
2025-06-14 16:43:33 -0700

    add cull face

--- src/main.py
+++ src/main.py
@@ -31,6 +31,9 @@ class Game(Application):
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
+        glEnable(GL_CULL_FACE)
+        glCullFace(GL_BACK);
+
         # Initialize Systems
         AnimationSystem.SetUp()
         GameObjectSystem.SetUp()
