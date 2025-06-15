2afaa9019251df884e11f3f95ee879eb587f2bdb
Joecheong2006 <joecheong2006@gmail.com>
2025-06-15 14:21:33 -0700

    changed frameIndex to frameIndexes

--- src/Animator.py
+++ src/Animator.py
@@ -11,41 +11,43 @@ class AnimationState:
         self.timeScale: float = timeScale
         self.loop: bool = loop
         self.finished: bool = False
-        self.frameIndex = 0
+        self.frameIndexes = [0] * len(self.anim.channels)
 
     def calculateAnimation(self, target: Model):
         if self.anim == None:
             return
 
-        for channel in self.anim.channels:
+        attr_map = {
+            'translation': 'translation',
+            'rotation': 'rotation',
+            'scale': 'scale',
+        }
+
+        for i, channel in enumerate(self.anim.channels):
             sampler = self.anim.samplers[channel.sampler]
             keyframe_times = sampler.keyframe_times
             keyframe_values = sampler.keyframe_values
 
-            tmp = [self.frameIndex]
-            interpolated_value = interp_anim_vec(channel.path, self.time, sampler.interpolation, keyframe_times, keyframe_values, tmp)
-            self.frameIndex = tmp[0]
+            interpolated_value, self.frameIndexes[i] = interp_anim_vec(
+                    channel.path, self.time, sampler.interpolation, 
+                    keyframe_times, keyframe_values, self.frameIndexes[i]
+                )
 
-            if channel.path == 'translation':
-                target.nodes[channel.node].translation = interpolated_value
-            elif channel.path == 'rotation':
-                target.nodes[channel.node].rotation = interpolated_value
-            elif channel.path == 'scale':
-                target.nodes[channel.node].scale = interpolated_value
+            setattr(target.nodes[channel.node], attr_map[channel.path], interpolated_value)
 
     def update(self, deltaTime: float):
         self.time += deltaTime * self.timeScale
         if self.time > self.duration:
             if self.loop:
                 self.time = np.fmod(self.time, self.duration)
-                self.frameIndex = 0
+                self.frameIndexes = [0] * len(self.anim.channels)
             else:
                 self.time = self.duration
                 self.finished = True
 
     def reset(self):
         self.time = 0
-        self.frameIndex = 0
+        self.frameIndexes = [0] * len(self.anim.channels)
         self.finished = False
 
 class Animator:
@@ -86,6 +88,7 @@ class Animator:
             return
 
         self.currentState.calculateAnimation(self.target)
+
         self.currentState.update(deltaTime)
 
         if not self.isTransitioning:
@@ -94,8 +97,7 @@ class Animator:
                     self.isTransitioning = True
                     self.transitionIndex = i
                     self.animationStates[transition.endAnimName].reset()
-
-        if self.isTransitioning:
+        else:
             transition = self.transitions[self.transitionIndex]
             if not transition.apply(deltaTime, self):
                 self.currentState.reset()
@@ -104,13 +106,16 @@ class Animator:
                 self.currentState = self.animationStates[transition.endAnimName]
                 transition.reset()
 
-        for node in self.target.nodes:
+        nodes = self.target.nodes
+        ordered_indexes = self.target.ordered_node_indexes
+
+        for node in nodes:
             node.transform = calc_local_transform(node)
-        for node_index in self.target.ordered_node_indexes:
-            node = self.target.nodes[node_index]
+        for node_index in ordered_indexes:
+            node = nodes[node_index]
             parent_index = node.parent_index
             if parent_index >= 0:
-                parent_node = self.target.nodes[parent_index]
+                parent_node = nodes[parent_index]
                 node.transform = parent_node.transform @ node.transform
 
         self.target.jointMatrices = calc_joint_matrices(self.target)
@@ -120,12 +125,10 @@ def calc_joint_matrices(model: Model):
         return []
     # assert len(model.skins) == 1
     for skin in model.skins:
-        joint_matrices = []
-        for joint_index, inverse_bind_matrix in zip(skin.joints, skin.inverse_bind_matrices):
-            node = model.nodes[joint_index]
-            joint_matrix = node.transform @ inverse_bind_matrix.reshape((4,4)).T
-            joint_matrices.append(joint_matrix)
-        return joint_matrices
+        return [
+                model.nodes[joint_index].transform @ inverse_bind_matrix
+                for joint_index, inverse_bind_matrix in zip(skin.joints, skin.inverse_bind_matrices)
+            ]
     return []
 
 def calc_local_transform(node):
@@ -138,28 +141,25 @@ def calc_local_transform(node):
         rotation = glm.mat4_cast(node.rotation)
     if hasattr(node, 'scale') and node.scale is not None:
         scale = glm.scale(scale, glm.vec3(node.scale))
-    transform = translation @ rotation @ scale
-    return np.array(transform)
+    return np.array(translation @ rotation @ scale)
 
 def interp_anim_vec(path, t: float, interpolation, keyframe_times, keyframe_values, frameIndex):
-    a, b, t = get_lerp(t, keyframe_times, frameIndex)
-    a = glm.quat(keyframe_values[a, [3,0,1,2]]) if path[0] == 'r' else glm.vec3(keyframe_values[a])
+    aIndex, b, t = get_lerp(t, keyframe_times, frameIndex)
+    a = glm.quat(keyframe_values[aIndex, [3,0,1,2]]) if path[0] == 'r' else glm.vec3(keyframe_values[aIndex])
     b = glm.quat(keyframe_values[b, [3,0,1,2]]) if path[0] == 'r' else glm.vec3(keyframe_values[b])
     if interpolation[0] == 'S':
-        return a
+        return a, aIndex
     elif interpolation[0] == 'L':
         if path[0] == 'r':
-            return glm.slerp(a, b, t)
-        return glm.lerp(a, b, t)
+            return glm.slerp(a, b, t), aIndex
+        return glm.lerp(a, b, t), aIndex
     assert False, 'bad interpolation'
 
 def get_lerp(t: float, keyframe_times, frameIndex):
     a = np.insert(np.array(keyframe_times, dtype=np.float32), 0, 0)
-    print(frameIndex[0])
-    for i in range(frameIndex[0], len(a) - 1):
+    for i in range(frameIndex, len(a) - 1):
         t0, t1, = a[i], a[i + 1]
         if t0 <= t < t1:
-            frameIndex[0] = i
             j = (i + 1) % len(keyframe_times)
             t = (t - t0) / (t1 - t0)
             return i, j, t
@@ -180,14 +180,15 @@ class AnimationTransition:
         endAnimState = states[self.endAnimName]
         endAnim = endAnimState.anim
 
-        for channel in endAnim.channels:
+        for i, channel in enumerate(endAnim.channels):
             sampler = endAnim.samplers[channel.sampler]
             keyframe_times = sampler.keyframe_times
             keyframe_values = sampler.keyframe_values
 
-            tmp = [endAnimState.frameIndex]
-            interpolated_value = interp_anim_vec(channel.path, endAnimState.time, sampler.interpolation, keyframe_times, keyframe_values, tmp)
-            endAnimState.frameIndex = tmp[0]
+            interpolated_value, endAnimState.frameIndexes[i] = interp_anim_vec(
+                    channel.path, endAnimState.time, sampler.interpolation, 
+                    keyframe_times, keyframe_values, endAnimState.frameIndexes[i]
+                )
 
             if channel.path == 'translation':
                 animator.target.nodes[channel.node].translation = glm.lerp(animator.target.nodes[channel.node].translation, interpolated_value, self.currentDuration)
