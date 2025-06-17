f56f61b8566dc1657af94a0a8477be410f07d733
Joecheong2006 <joecheong2006@gmail.com>
2025-06-17 14:10:10 -0700

    add collision AABB test and changed model

--- src/Player.py
+++ src/Player.py
@@ -7,11 +7,40 @@ from Animator import Animator
 from pyglm import glm
 from Camera import Camera
 
+from collections import namedtuple
+AABB = namedtuple('AABB', 'min max')
+
+def aabbCollision(boxA: AABB, boxB: AABB):
+    return (
+        boxA.min[0] <= boxB.max[0] and boxA.max[0] >= boxB.min[0] and
+        boxA.min[1] <= boxB.max[1] and boxA.max[1] >= boxB.min[1] and
+        boxA.min[2] <= boxB.max[2] and boxA.max[2] >= boxB.min[2]
+    )
+
+def resolve_aabb_collision(boxA, boxB):
+    dx1 = boxB.max[0] - boxA.min[0]
+    dx2 = boxA.max[0] - boxB.min[0]
+    dy1 = boxB.max[1] - boxA.min[1]
+    dy2 = boxA.max[1] - boxB.min[1]
+    dz1 = boxB.max[2] - boxA.min[2]
+    dz2 = boxA.max[2] - boxB.min[2]
+
+    mtv_x = dx1 if dx1 < dx2 else -dx2
+    mtv_y = dy1 if dy1 < dy2 else -dy2
+    mtv_z = dz1 if dz1 < dz2 else -dz2
+
+    abs_mtv = [(abs(mtv_x), (mtv_x, 0, 0)),
+               (abs(mtv_y), (0, mtv_y, 0)),
+               (abs(mtv_z), (0, 0, mtv_z))]
+    min_mtv = min(abs_mtv, key=lambda x: x[0])[1]
+
+    return min_mtv 
+
 class Player(GameObject):
     def __init__(self):
         super().__init__(self)
 
-        self.model = Model("res/M.glb")
+        self.model = Model("res/N.glb")
 
         self.forward: glm.vec3 = glm.vec3(0, 0, 1)
         self.right: glm.vec3 = glm.vec3(1, 0, 0)
@@ -48,6 +77,13 @@ class Player(GameObject):
         self.animator.addTransition("Idle", "Sitting", 0.3, StartSitting)
         self.animator.addTransition("Sitting", "Idle", 0.3, EndSitting)
 
+        self.aabb = AABB(self.transform.position + glm.vec3(-0.3, 0, -0.3), self.transform.position + glm.vec3(0.3, 2, 0.3))
+
+        sitLocation = glm.vec3(-2, 0, -1)
+        self.aabbBoxes: list[AABB] = [ 
+                AABB(sitLocation + glm.vec3(-1.8, 0, -1.2), sitLocation + glm.vec3(0.6, 1, 0.5))
+            ]
+
     def followCameraDirection(self, cam: Camera):
         self.forward = -glm.normalize(glm.vec3(cam.forward()) * glm.vec3(1, 0, 1))
         self.right = glm.normalize(glm.vec3(cam.right()) * glm.vec3(1, 0, 1))
@@ -55,6 +91,12 @@ class Player(GameObject):
     def OnUpdate(self, window: Window):
         deltaTime = window.deltaTime
         keys = window.keys
+
+        self.aabb = AABB(self.transform.position + glm.vec3(-0.3, 0, -0.3), self.transform.position + glm.vec3(0.3, 2, 0.3))
+
+        for box in self.aabbBoxes:
+            if aabbCollision(self.aabb, box):
+                self.transform.position += resolve_aabb_collision(self.aabb, box)
 
         self.sitting = keys[pg.K_SPACE]
         if self.animator.variables["sitTransition"]:
