1ed70f4918cc75117cbd836f93fc596cfb5b1353
Joecheong2006 <joecheong2006@gmail.com>
2025-06-17 03:55:14 -0700

    remove gamma correction

--- src/main.py
+++ src/main.py
@@ -126,7 +126,6 @@ class Game(Application):
                     }
 
                     vec3 color = texture(screenTexture, TexCoord).rgb * diff;
-                    color = pow(color.rgb, vec3(1.0 / 2));
                     fragColor = vec4(color, 1);
                 }
                 """
